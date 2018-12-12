8115263f3c9ba119b977ed14ecce6a77e398fcd6
Apparum <dorian.pery@gmail.com>
2018-12-12 12:17:47 +0100

    Two windows communicating

--- app/src/main/java/projetandroid/fise3/tse/projetandroid/FormActivity.java
+++ app/src/main/java/projetandroid/fise3/tse/projetandroid/FormActivity.java
@@ -1,13 +1,46 @@
 package projetandroid.fise3.tse.projetandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
+
+    private Button confirmButton;
+    private EditText editTextName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+
+        initView();
+    }
+
+    private void initView() {
+        confirmButton = findViewById(R.id.activity_form_button_confirm);
+        editTextName = findViewById(R.id.activity_form_editText_name);
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                confirmName();
+            }
+        });
+    }
+
+    private void confirmName() {
+        String name = editTextName.getText().toString();
+        if (!name.isEmpty()){
+            DataManager.getInstance().addItem(name);
+        } else {
+            Toast.makeText(this,
+                    "Veuillez saisir votre nom",
+                    Toast.LENGTH_SHORT).show();
+        }
+        finish();
     }
 }
--- app/src/main/java/projetandroid/fise3/tse/projetandroid/MainActivity.java
+++ app/src/main/java/projetandroid/fise3/tse/projetandroid/MainActivity.java
@@ -4,33 +4,47 @@ import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    TextView textViewName;
     Button buttonEditName;
+    RecyclerView recyclerView;
+    NameListAdapter nameListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        initializeFields();
+        initViews();
+        initList();
     }
 
-    private void initializeFields()
+    @Override
+    protected void onResume() {
+        super.onResume();
+        nameListAdapter.updateList(DataManager.getInstance().getNameList());
+    }
+
+    private void initViews()
     {
-        textViewName = findViewById(R.id.activity_main_textView_name);
+        recyclerView = findViewById(R.id.activity_main_recyclerView);
         buttonEditName = findViewById(R.id.activity_main_button_editName);
         buttonEditName.setOnClickListener(this);
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    private void initList(){
+        nameListAdapter = new NameListAdapter();
+        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation((LinearLayoutManager.VERTICAL));
+
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(nameListAdapter);
     }
 
     @Override
@@ -38,7 +52,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (view.getId()){
             case R.id.activity_main_button_editName:
                 Intent intent = new Intent(this, FormActivity.class);
-                startActivityForResult(intent, 1);
+                startActivity(intent);
                 break;
         }
     }
